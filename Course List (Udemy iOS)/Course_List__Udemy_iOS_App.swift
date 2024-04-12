f196cd2f5022d57b299e261bb503b26bed285df9
Pedro Marques <pdro.marqs2015@hotmail.com>
2024-04-12 19:35:12 -0300

    Initial Commit

---
+++ Course List (Udemy iOS)/Course_List__Udemy_iOS_App.swift
@@ -0,0 +1,17 @@
+//
+//  Course_List__Udemy_iOS_App.swift
+//  Course List (Udemy iOS)
+//
+//  Created by Pedro Marques on 12/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Course_List__Udemy_iOS_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
